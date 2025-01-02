6c606ad4ca707c1d5689cf25f5b21ab7de103072
julia-ln <julia.lechner@stud.fh-campuswien.ac.at>
2025-01-02 14:40:31 +0100

    WordProvider und Controller für start-view.fxml

--- src/main/java/at/ac/fhcampuswien/hangmanmaven/HangmanController.java
+++ src/main/java/at/ac/fhcampuswien/hangmanmaven/HangmanController.java
@@ -1,8 +1,45 @@
 package at.ac.fhcampuswien.hangmanmaven;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class HangmanController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HangmanController implements Initializable{
+    @FXML
+    private ChoiceBox<String> categoryButton;
+
+    private WordProvider wordProvider;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        wordProvider = new WordProvider();
+
+        categoryButton.getItems().addAll(wordProvider.getCategories());
+    }
+
+    public void onPlayButtonClick(){
+        String selectedCategory = categoryButton.getValue();
+        if (selectedCategory != null){
+            try {
+                Stage currentStage = (Stage) categoryButton.getScene().getWindow();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("hangman-view.fxml"));
+                AnchorPane root = loader.load();
+                Scene scene = new Scene(root);
+
+                currentStage.setScene(scene);
+                currentStage.close();
+                currentStage.show();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
--- src/main/java/at/ac/fhcampuswien/hangmanmaven/WordProvider.java
+++ src/main/java/at/ac/fhcampuswien/hangmanmaven/WordProvider.java
@@ -1,4 +1,34 @@
 package at.ac.fhcampuswien.hangmanmaven;
 
+import java.util.*;
+
 public class WordProvider {
+    private final Map<String, List<String>> wordCategories;
+
+    public WordProvider() {
+        wordCategories = new HashMap<>();
+
+        wordCategories.put("Animals", Arrays.asList("Dog","Cat","Elephant","Lion","Tiger","Monkey","Giraffe","Donkey","Goat","Goose","Horse","Sheep","Turtle","Zebra","Dolphin","Kangaroo","Snake","Eagle","Whale","Octopus","Butterfly","Spider","Mouse","Rabbit","Squirrel"));
+        wordCategories.put("Countries", Arrays.asList("Austria","Germany","France","Spain","Denmark","England","Canada","Finland","Sweden","Norway","Italy","Netherlands","Greece","India","Ireland","Iceland","Japan","China","Croatia","Mexico","Poland","Russia","Switzerland","Hungary","Portugal","Belgium","Australia"));
+        wordCategories.put("Sports", Arrays.asList("Basketball","Football","Tennis","Badminton","Baseball","Bowling","Boxing","Climbing","Cycling","Golf","Hockey","Handball","Hiking","Skiing","Surfing","Swimming","Volleyball","Diving","Darts","Running"));
+        wordCategories.put("Colours", Arrays.asList("Black","Blue","Green","Yellow","Orange","Purple","Pink","Brown","Grey","Turquoise","Khaki","Azure","Cyan","Magenta","Beige"));
+        wordCategories.put("Fruits", Arrays.asList("Pineapple","Apple","Banana","Cherry","Orange","Pear","Blackberry","Blueberry","Strawberry","Raspberry","Kiwi","Coconut","Mango","Watermelon","Peach","Grape","Lemon","Plum","Lychee","Dragonfruit","Pomegranate","Papaya"));
+    }
+
+    public List<String> getCategories(){
+        return new ArrayList<>(wordCategories.keySet());
+    }
+
+    public String getRandomWord(String category){
+        List<String> words = wordCategories.get(category);
+        if (words == null || words.isEmpty()){
+            return null;
+        }
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+
+    public List<String> getWordsForCategory(String category){
+        return wordCategories.getOrDefault(category, Collections.emptyList());
+    }
 }
